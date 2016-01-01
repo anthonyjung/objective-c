a755f671dc88340234ede0ea7bc9cdea59548b53
anthonyjung <lazybricker@gmail.com>
2016-01-01 17:27:54 +0900

    Add Hello World Project

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by 원재 정 on 2016. 1. 1..
+//  Copyright © 2016년 Anthony Jung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello World.");
+    }
+    return 0;
+}
