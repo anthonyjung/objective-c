8e3564ef87e394cd733fac9a1bd5525050fa5102
anthonyjung <lazybricker@gmail.com>
2016-01-01 17:34:30 +0900

    Add Rectangle class

---
+++ Rectangle/Rectangle/Rectangle.h
@@ -0,0 +1,13 @@
+//
+//  Rectangle.h
+//  Rectangle
+//
+//  Created by 원재 정 on 2016. 1. 1..
+//  Copyright © 2016년 Anthony Jung. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface Rectangle : NSObject
+
+@end
---
+++ Rectangle/Rectangle/Rectangle.m
@@ -0,0 +1,13 @@
+//
+//  Rectangle.m
+//  Rectangle
+//
+//  Created by 원재 정 on 2016. 1. 1..
+//  Copyright © 2016년 Anthony Jung. All rights reserved.
+//
+
+#import "Rectangle.h"
+
+@implementation Rectangle
+
+@end
