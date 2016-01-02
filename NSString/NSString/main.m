5d82ac260216428636d6ed2e3884acc159cd0201
anthonyjung <lazybricker@gmail.com>
2016-01-02 22:41:27 +0900

    Create NSString project

---
+++ NSString/NSString/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NSString
+//
+//  Created by 원재 정 on 2016. 1. 2..
+//  Copyright © 2016년 Anthony Jung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
