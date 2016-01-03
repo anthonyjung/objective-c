b9c94d3874713643f3e6612196af53ce1f852832
anthonyjung <lazybricker@gmail.com>
2016-01-03 22:27:08 +0900

    Create NSArray project

---
+++ NSArray/NSArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  NSArray
+//
+//  Created by 원재 정 on 2016. 1. 3..
+//  Copyright © 2016년 Anthony Jung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
